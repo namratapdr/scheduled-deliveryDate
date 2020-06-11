8701dff138d4040183ea14070bb68aa85589cf7e
namratapdr <namratapodder138@gmail.com>
2020-06-11 14:39:36 +0530

    Connected to a different database now

---
+++ DBconfig/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "namratap_writesoft-projects";
+$password = "KaiatBkNr";
+$dbname = "namratap_writesoft-projects"
+    // Create connection
+    $con = mysqli_connect("localhost","namratap_writesoft-projects","KaiatBkNr","namratap_writesoft-projects");
+
+        if (mysqli_connect_error()) {
+
+            die ("Database Connection Error");
+
+        }
+
+?>
